ed38496794423d31e0eb1d36d197b26aff8837cb
Jose Paiva <jc0d3001@outlook.com>
2020-08-03 15:11:37 -0300

    gg

--- client/src/pages/logout.vue
+++ client/src/pages/logout.vue
@@ -1,8 +1,13 @@
+<template>
+<div>
+</div>
+</template>
 <script>
 export default {
     created(){
-       this.$store.dispatch('singOut', this.user);
-       this.$router.push('/'); 
+       this.$store.dispatch('singOut', this.user).then(()=>{
+           this.$router.push('/');
+       });       
     }
 }
 </script>
